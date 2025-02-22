e4420ede0ba047268414740bb1b7c3bec87e062f
Alexander <alexandergaruba96@gmail.com>
2025-02-22 20:20:35 +0100

    Refactor store and CartSlice; update App and AboutUs components for branding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import React, { useState } from 'react'
+import AboutUs from './AboutUs'
+import './App.css'
+import ProductList from './ProductList'
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -18,10 +17,10 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Welcome To Alexander's Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
-         
+
           <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
           </button>
@@ -40,6 +39,3 @@ function App() {
 }
 
 export default App;
-
-
-
